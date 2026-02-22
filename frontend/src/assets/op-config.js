6693e8f82b07139839e931e90e468a2ef616bb5c
Roy Nouneh <roy.nouneh@gmail.com>
2026-02-22 18:38:15 -0500

    Commit v0.0.2-002 - Realtime backbone on AWS

--- frontend/src/assets/op-config.js
+++ frontend/src/assets/op-config.js
@@ -2,8 +2,8 @@ window.__OP_CONFIG__ = {
   apiBaseUrl: 'https://4j8meghx2i.execute-api.us-east-1.amazonaws.com',
   storageMode: 'remote', // 'local' | 'remote'
   storageApiBaseUrl: 'https://4j8meghx2i.execute-api.us-east-1.amazonaws.com',
-  realtimeEnabled: false,
-  realtimeWsUrl: '', // e.g. wss://your-realtime-endpoint.example/ws
+  realtimeEnabled: true,
+  realtimeWsUrl: 'wss://kor4dh9vtl.execute-api.us-east-1.amazonaws.com/prod',
   mockMode: true,
   guestModeOnly: false,
   debugPerf: false,
